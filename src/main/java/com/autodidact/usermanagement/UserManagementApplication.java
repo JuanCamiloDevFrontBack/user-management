c48576100f98a3b2ee3380bad72c0c0d2404cac6
JuanCamilo9915 <jcamiloduque0215@gmail.com>
2024-01-17 23:07:03 -0500

    Se realizan cambios en el controllador de login el cual no esta terminado pero se estarán realizando ajustes en el mismo, adicional se deja solucionado el tema de cors para ese controlador con la anotación @CrossOrigin.

--- src/main/java/com/autodidact/usermanagement/UserManagementApplication.java
+++ src/main/java/com/autodidact/usermanagement/UserManagementApplication.java
@@ -2,7 +2,6 @@ package com.autodidact.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class UserManagementApplication {
--- src/main/java/com/autodidact/usermanagement/module/login/controller/LoginController.java
+++ src/main/java/com/autodidact/usermanagement/module/login/controller/LoginController.java
@@ -1,12 +1,31 @@
 package com.autodidact.usermanagement.module.login.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/login")
-@CrossOrigin(origins = {"*"})
 public class LoginController {
-
+	
+	@PostMapping
+	public ResponseEntity<Map<String, String>> requestLoginUser(@RequestBody String user) {
+		System.out.println("post user: " + user);
+		 Map<String, String> map = new HashMap<>();
+		  map.put("user", user);
+		/* Opción # 1
+		 * return new ResponseEntity<>(map, HttpStatus.OK);
+		 * */
+		// Opción # 2
+		return ResponseEntity.status(HttpStatus.OK).body(map);
+	}
+	
 }
