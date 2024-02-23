c2e314d2e1ac921e8768fab4c7d0d590ff65591f
JuanCamilo9915 <jcamiloduque0215@gmail.com>
2024-02-23 00:04:10 -0500

    Se agrega el método compareToUser en LoginServices y se hacen cambios pequeños.

--- src/main/java/com/autodidact/usermanagement/module/login/service/LoginService.java
+++ src/main/java/com/autodidact/usermanagement/module/login/service/LoginService.java
@@ -7,37 +7,66 @@ import com.autodidact.usermanagement.exception.CustomException;
 import com.autodidact.usermanagement.module.login.dao.ILoginDAO;
 import com.autodidact.usermanagement.module.login.dto.LoginDTOReq;
 import com.autodidact.usermanagement.module.login.dto.LoginDTORes;
+import com.autodidact.usermanagement.module.login.entity.LoginDAOEntity;
 
 @Service
 public class LoginService {
-	
+
 	@Autowired
 	private ILoginDAO loginDao;
-	
-	public LoginDTORes accesValid(LoginDTOReq user) {
-		
+
+	public LoginDTORes accesValidTest(LoginDTOReq user) {
+
 		var listUsingWhere = this.loginDao.searchByUserFirst();
 		System.out.println("list con where: " + listUsingWhere);
 		System.out.println("-----------------");
-		
+
 		var listUsingWhereJPQL = this.loginDao.searchByIdQueryJPQL(3);
 		System.out.println("list con where JPQL: " + listUsingWhereJPQL);
 		System.out.println("-----------------");
-		
+
 		var listUsers = this.loginDao.findAll();
 		System.out.println("Users DB: " + listUsers);
-		
+
 		var x = listUsers.get(0);
 		System.out.println("Users DB: " + x.getEmail());
 		System.out.println("Users DB: " + x.getEmail().equals(user.user()));
 		System.out.println("Users DB: " + x.getPassword().equals(user.password()));
 		var y = x.getEmail().equals(user.user()) && x.getPassword().equals(user.password());
-		
+
 		if (y) {
 			return new LoginDTORes("eabc.yt-dfjklfj48sadfs.sadiojhhr", true);
 		} else {
 			throw new CustomException("Error al realizar Login");
 		}
+	}
+
+	public LoginDTORes accesValid(LoginDTOReq user) {
+		
+		var listUsers = this.loginDao.findAll();
+		var isLogged = false;
+		System.out.println("Users DB: " + listUsers);
+
+		for (LoginDAOEntity isUser : listUsers) {
+			System.out.println("User: " + isUser);
+			isLogged = this.compareToUser(isUser, user);			
+			if (isLogged) { break; }
+		}
+		
+		if (isLogged) {
+			return new LoginDTORes("eabc.yt-dfjklfj48sadfs.sadiojhhr", isLogged);
+		} else {
+			throw new CustomException("Usuario y/o Contraseña incorrectos.");
+		}
+
+	}
+	
+	private Boolean compareToUser(LoginDAOEntity userDB, LoginDTOReq isUserValid) {
+		return userDB.getEmail()
+				.equals(isUserValid.user())
+				&&
+				userDB.getPassword()
+				.equals(isUserValid.password());
 	}
 
 }
